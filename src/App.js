186c7c408aa74a4be593ce3fc24954c12e9fc380
Miranda Hawkes <17408278+miranda-hawkes@users.noreply.github.com>
2020-12-09 12:17:26 +0000

    Added React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Container className="p-3">
+    <Jumbotron>
+      <h1 className="header">Corn Trip Cost Calculator</h1>
+
+      <Row className="justify-content-md-center">
+        <Col xs lg="4">
+            <InputGroup className="mb-3">
+            <FormControl
+              placeholder="Enter number of bags"
+              aria-label="Number of bags"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+
+    </Jumbotron>
+  </Container>
+);
 
 export default App;
