5807fff2480cc3c052f5159565d0ea7e3d4b6e99
Miranda Hawkes <17408278+miranda-hawkes@users.noreply.github.com>
2020-12-09 13:09:31 +0000

    Calculating cost of trip

--- src/App.js
+++ src/App.js
@@ -1,33 +1,80 @@
-import React, { useState } from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import FormControl from 'react-bootstrap/FormControl';
-import InputGroup from 'react-bootstrap/InputGroup';
+import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
-const App = () => (
-  <Container className="p-3">
-    <Jumbotron>
+function App() {
+  const [costOfTrip, setCostOfTrip] = useState(0);
+  const [clear, setClear] = useState(false);
+
+  useEffect( () => {
+    document.querySelector('#result').value = "";
+  },[])
+  
+  useEffect( () => {
+    if(clear)
+    document.querySelector('#result').value = "";
+  })
+
+  const Calculate = (e) => {
+    e.preventDefault();
+    if(clear) setClear(false);
+    let numBags = document.querySelector('#num-bags').value
+    if(numBags == '')
+    return;
+    let sum = numBags * 0.25
+    setCostOfTrip(sum);
+    document.querySelector('#num-bags').value = "";
+  }
+
+  return (
+
+    <Container>
+
       <h1 className="header">Corn Trip Cost Calculator</h1>
+    
+      <Container>
 
-      <Row className="justify-content-md-center">
-        <Col xs lg="4">
-            <InputGroup className="mb-3">
-            <FormControl
-              placeholder="Enter number of bags"
-              aria-label="Number of bags"
-              aria-describedby="basic-addon1"
-            />
-          </InputGroup>
-        </Col>
-      </Row>
+        <Form>
+          <Row className="justify-content-md-center">
+            <Col xs lg="4">
+              <Form.Group controlId="num-bags">
+                <Form.Control type="number" placeholder="Enter number of bags of corn" />
+              </Form.Group>
+            </Col>
+          </Row>
 
-    </Jumbotron>
-  </Container>
-);
+          <Row className="justify-content-md-center">
+            <Col xs lg="4">
+              <Button variant="primary" type="submit" onClick={Calculate}>
+                Calculate
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+
+      <Container>
+
+        <Form>
+          <Row className="justify-content-md-center">
+
+            <Col xs lg="4">
+
+              <Form.Label >
+                At a cost of
+              </Form.Label>
+              <Form.Control plaintext readOnly defaultValue="email@example.com" id="result" value={'£' + costOfTrip}/>
+
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </Container> 
+  );
+}
 
 export default App;
